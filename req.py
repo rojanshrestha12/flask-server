2dc5b56608ef91d39d09cd919c16c4fc0dba8a36
rojanstha621 <rojanstha621@gmail.com>
2025-06-08 18:34:18 +0545

    shit

--- req.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://localhost:5000/upload"
-files = {'file': open('password.txt', 'rb')}
-
-res = requests.post(url, files=files)
-print("Status:", res.status_code)
-print("Response:", res.text)
